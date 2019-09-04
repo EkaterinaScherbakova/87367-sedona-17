52bc1975fab53d0a4aa9f5f76bbd6c8b1221fa46
Ekaterina Scherbakova <katarin08@gmail.com>
2019-09-04 23:19:31 +0300

    попапы

--- source/js/main.js
+++ source/js/main.js
@@ -22,3 +22,38 @@
     }
   });
 })()
+
+;(function() {
+  var success = document.getElementById('popup-success');
+  var error = document.getElementById('popup-error');
+  if (!success || !error) return;
+
+  var successClose = success.querySelector('button');
+  var errorClose = error.querySelector('button');
+  var form = document.getElementById('review-form');
+  var submit = form.querySelector('[type=submit]');
+
+  successClose.addEventListener('click', closeModals);
+  errorClose.addEventListener('click', closeModals);
+
+  window.addEventListener('keydown', function (evt) {
+      if (evt.keyCode === 27) closeModals(evt);
+  });
+
+  submit.addEventListener('click', function (evt) {
+      if (form.checkValidity()) {
+        evt.preventDefault();
+        if (!success.classList.contains('popup--show')) success.classList.add('popup--show');
+        if (error.classList.contains('popup--show')) error.classList.remove('popup--show');
+      } else {
+        if (success.classList.contains('popup--show')) success.classList.remove('popup--show');
+        if (!error.classList.contains('popup--show')) error.classList.add('popup--show');
+      }
+  });
+
+  function closeModals(evt) {
+    evt.preventDefault();
+    if (success.classList.contains('popup--show')) success.classList.remove('popup--show');
+    if (error.classList.contains('popup--show')) error.classList.remove('popup--show');
+  }
+})()
